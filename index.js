28f7ed4984eb770c5daf9e23e952d6a0c107d61a
kzlsakal <dorukgurleyen@gmail.com>
2020-05-03 18:37:34 +0000

    delete .replit

--- index.js
+++ index.js
@@ -3,4 +3,12 @@
 // this is the same as print
 console.log('hello world')
 
-// test 2
+//you can throw random values in the code, it does not break the compiler.
+"a string" 
+2321
+
+// define var using = sign 
+// order is important like in Phython
+var myprint = "another string"
+var myprint2 = ""
+console.log(myprint)

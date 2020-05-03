ec5a626b651c37bbacdba8bd8d27653b289e10f5
kzlsakal <dorukgurleyen@gmail.com>
2020-05-03 19:11:31 +0000

    0.0.0.003

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 // comment
 
 // this is the same as print
-console.log('hello world')
+console.log('hello world');
 
 //you can throw random values in the code, it does not break the compiler.
-"a string" 
-2321
+"a string" ;
+2321;
 
 // define var using = sign 
 // order is important like in Phython
-var myprint = "another string"
-var myprint2 = ""
-console.log(myprint)
+var myprint = "another string";
+var myprint2 = " and another";
+console.log(myprint + myprint2);
+
+// math
+console.log(4*2);
+
+// if both conditions are true
+console.log(true && true);
+console.log(true && false);
+
+// if one of the conditions is true
+console.log(true || false);
+console.log(false || false);
+
+// check if equal with ===
+console.log(870 === 870);
+console.log(870 === 875);
+console.log('red' === 'red');
+console.log('red' === 'yellow');
+
+// if statements
+
+var a = 45;
+var b = 45;
+var c = 15
+
+if (b === a) {
+console.log('I am equal');
+}
+if (c < a) {
+console.log('I am smaller');
+}
+
+// while statements
+
